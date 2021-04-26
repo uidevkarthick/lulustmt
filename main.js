595a74758f3fea7e26fcf33986c71c3512da82ac
shopeetale <myshopeetale@gmail.com>
2021-04-26 13:12:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function populate(param) {
+    var myelement = document.getElementById("date");
+    myelement.innerHTML = param;
+}
+function populate1(param) {
+    var myelement = document.getElementById("test1");
+    myelement.innerHTML = param;
+}
+function populate2(param) {
+    var myelement = document.getElementById("test2");
+    myelement.innerHTML = param;
+}
